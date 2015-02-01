0109069c170b598cdd496e6774969511daeae3db
Bl4ckHole <corentinja@gmail.com>
2015-02-01 19:04:42 +0100

    Trigger for the nuke (hearts !)

--- src/main/java/com/ceselegend/rozmod/block/RozFatMan.java
+++ src/main/java/com/ceselegend/rozmod/block/RozFatMan.java
@@ -8,6 +8,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -47,5 +48,17 @@ public class RozFatMan extends BlockContainer {
     @SideOnly(Side.CLIENT)
     public void registerBlockIcons(IIconRegister iconRegister) {
         blockIcon = iconRegister.registerIcon(Reference.MOD_ID+":rozFatMan");
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
+        TileEntityFatMan bomb = (TileEntityFatMan) world.getTileEntity(x,y,z);
+        if(!bomb.getPrimed()) {
+            bomb.setPrimed();
+            return true;
+        }else {
+            return super.onBlockActivated(world,x,y,z,player,p_149727_6_,p_149727_7_,p_149727_8_,p_149727_9_);
+        }
+
     }
 }
--- src/main/java/com/ceselegend/rozmod/entity/TileEntityFatMan.java
+++ src/main/java/com/ceselegend/rozmod/entity/TileEntityFatMan.java
@@ -1,11 +1,57 @@
 package com.ceselegend.rozmod.entity;
 
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityFatMan extends TileEntity {
 
-    public TileEntityFatMan() {
+    private int fuse;
+    private boolean primed;
 
+    public TileEntityFatMan() {
+        fuse = 100;
+        primed = false;
+    }
+
+    public void setPrimed() {
+        this.primed = true;
+    }
+
+    public boolean getPrimed() {
+        return primed;
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound coumpound) {
+        super.writeToNBT(coumpound);
+
+        coumpound.setShort("fuse",(short)fuse);
+        coumpound.setBoolean("primed",primed);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound coumpond) {
+        super.readFromNBT(coumpond);
+
+        fuse = coumpond.getShort("fuse");
+        primed = coumpond.getBoolean("primed");
+    }
+
+    @Override
+    public void updateEntity() {
+        if(!worldObj.isRemote) {
+            if(this.primed && fuse > 0) {
+                this.fuse -= 1;
+
+            }else if(fuse == 0){
+                worldObj.setBlockToAir(xCoord,yCoord,zCoord);
+                worldObj.removeTileEntity(xCoord,yCoord,zCoord);
+            }
+        }else {
+            if(primed){
+                worldObj.spawnParticle("heart",(double)xCoord+worldObj.rand.nextDouble(),(double)yCoord+1,(double)zCoord+worldObj.rand.nextDouble(),0,0.5,0);
+            }
+        }
     }
 }
